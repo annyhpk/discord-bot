89ce9a012842a8ad0333003a89b48d5bb9c052b8
Chanho Kim <cksgh063@gmail.com>
2020-06-07 17:16:08 +0900

    update

--- dist/parser/date.js
+++ dist/parser/date.js
@@ -1,7 +1,7 @@
 import dateData from './data/date.json';
-export var parser = function () {
-    var date = new Date();
-    var today = date.getMonth() + 1 + "-" + date.getDate();
+export const parser = () => {
+    const date = new Date();
+    const today = `${date.getMonth() + 1}-${date.getDate()}`;
     console.log('✅ 날짜 파싱 완료');
     return dateData[today] || null;
 };
