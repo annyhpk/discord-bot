3f4a5231d24972e4b63333a01d0eb73a9bd2bd08
Chanho Kim <cksgh063@gmail.com>
2020-06-07 20:07:30 +0900

    fixed

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,7 @@ import parser from './parser';
 import discord from './lib/discord';
 
 (async () => {
-  // const WEBHOOKS = process.env.WEBHOOKS;
-  const WEBHOOKS = "https://discordapp.com/api/webhooks/714403752609579051/NPJAa-BUckzNRLJjTd1NVuh1L8G1pLQYffMRw7f5CPM4Pj5igE1Imd3i9zBMQdPR6E-S";
+  const WEBHOOKS = process.env.WEBHOOKS;
   if (WEBHOOKS == null) throw new Error('웹훅 리스트를 찾을 수 없어요.');
 
   const webhookList = WEBHOOKS.split(',');
