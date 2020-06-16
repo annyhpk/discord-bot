ad65980195aebbc14ad233c5335f1cc5aeb8dd2a
Chanho Kim <cksgh063@gmail.com>
2020-06-16 10:22:13 +0900

    update

--- dist/lib/discord.js
+++ dist/lib/discord.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,39 +8,82 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import axios from 'axios';
-export default ({ weather, news, date, url }) => __awaiter(void 0, void 0, void 0, function* () {
-    const today = new Date().toLocaleDateString().replace(/\. /g, '-').replace('.', '');
-    let message = {
-        username: '알림봇',
-        content: `📨 ${today} 오늘이 시작됐어요!`,
-        embeds: [],
-    };
-    message.embeds.push({
-        fields: [
-            {
-                name: '📅 날짜 / 한국',
-                value: `${today} ${date ? '(' + date + ')' : ''}`,
-                inline: true
-            },
-            {
-                name: '🏞️ 날씨 / 삼례',
-                value: weather.weather,
-                inline: true
-            },
-            {
-                name: '🌡 온도 / 삼례',
-                value: weather.temp,
-                inline: true
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
             }
-        ],
-        footer: {
-            text: '제작자 : annyhpk'
-        },
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+exports.default = (function (_a) {
+    var weather = _a.weather, news = _a.news, date = _a.date, url = _a.url;
+    return __awaiter(void 0, void 0, void 0, function () {
+        var today, message;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    today = new Date().toLocaleDateString().replace(/\. /g, '-').replace('.', '');
+                    message = {
+                        username: '알림봇',
+                        content: "\uD83D\uDCE8 " + today + " \uC624\uB298\uC774 \uC2DC\uC791\uB410\uC5B4\uC694!",
+                        embeds: [],
+                    };
+                    message.embeds.push({
+                        fields: [
+                            {
+                                name: '📅 날짜 / 한국',
+                                value: today + " " + (date ? '(' + date + ')' : ''),
+                                inline: true
+                            },
+                            {
+                                name: '🏞️ 날씨 / 삼례',
+                                value: weather.weather,
+                                inline: true
+                            },
+                            {
+                                name: '🌡 온도 / 삼례',
+                                value: weather.temp,
+                                inline: true
+                            }
+                        ],
+                        footer: {
+                            text: '제작자 : annyhpk'
+                        },
+                    });
+                    message.embeds.push({
+                        title: '📰 뉴스 / 구글',
+                        description: news
+                    });
+                    return [4 /*yield*/, axios_1.default.post(url, message)];
+                case 1:
+                    _b.sent();
+                    return [2 /*return*/];
+            }
+        });
     });
-    message.embeds.push({
-        title: '📰 뉴스 / 구글',
-        description: news
-    });
-    yield axios.post(url, message);
 });
--- dist/parser/date.js
+++ dist/parser/date.js
@@ -1,7 +1,13 @@
-import dateData from './data/date.json';
-export const parser = () => {
-    const date = new Date();
-    const today = `${date.getMonth() + 1}-${date.getDate()}`;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.parser = void 0;
+var date_json_1 = __importDefault(require("./data/date.json"));
+exports.parser = function () {
+    var date = new Date();
+    var today = date.getMonth() + 1 + "-" + date.getDate();
     console.log('✅ 날짜 파싱 완료');
-    return dateData[today] || null;
+    return date_json_1.default[today] || null;
 };
